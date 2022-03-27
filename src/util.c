62ce74d31e41b53c0d3b2cc1f7503e1d6b231253
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-03-27 11:56:02 +0200

    util: remove unused include files

--- src/util.c
+++ src/util.c
@@ -4,15 +4,6 @@
  * Copyright (C) 2010 Alfred E. Heggestad
  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
-#ifdef HAVE_IO_H
-#include <io.h>
-#endif
 #include <string.h>
 #include <re.h>
 #include <baresip.h>
